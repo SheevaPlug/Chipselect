e303a87a4d018012bbace9487c4f8b0ff2da5217
Lutz Badenheuer <software@lukenukem.de>
2021-09-15 20:25:39 +0200

    added detail view

--- docker/files/web/app/routes.py
+++ docker/files/web/app/routes.py
@@ -18,3 +18,10 @@ def index():
     else:
         print(form.errors)
     return render_template('index.html', form=form, results=results)
+
+
+@app.route('/show/<deviceid>')
+def show(deviceid):
+    result = app.search.get_source(index='mcs', id=deviceid)
+    return render_template('show.html', result=result)
+    
