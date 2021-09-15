0e2d850d9d53be17fbde8618b2c3d5d082e9f229
Lutz Badenheuer <software@lukenukem.de>
2021-09-15 14:01:33 +0200

    added rudimentary search

--- docker/files/web/app/routes.py
+++ docker/files/web/app/routes.py
@@ -7,8 +7,14 @@ from app.forms import SearchForm
 @app.route('/index', methods=['GET', 'POST'])
 def index():
     form = SearchForm()
+    results = []
     if form.validate_on_submit():
-        print(form.query)
+        #print(form.query.data)
+        #print(app.search.search(body={'query': {'match_all': form.query.data}}))
+        results = app.search.search(
+            index='mcs',
+            body={'query': {'query_string': {'query': form.query.data}}})
+        #print(results)
     else:
         print(form.errors)
-    return render_template('index.html', form=form)
+    return render_template('index.html', form=form, results=results)
