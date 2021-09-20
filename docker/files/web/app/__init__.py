ca41123e50aecdb85f3ec432b7d34fce0a9dc41d
Lutz Badenheuer <software@lukenukem.de>
2021-09-20 20:57:43 +0200

    now running with docker-compose

--- docker/files/web/app/__init__.py
+++ docker/files/web/app/__init__.py
@@ -1,9 +1,11 @@
+import os
+
 from flask import Flask
 from jinja2.tests import test_mapping, test_sequence
 from elasticsearch import Elasticsearch
 
 app = Flask(__name__)
-app.config.from_object('app.config')
+app.config.from_object(os.getenv('CONFIGFILE', 'app.config-development'))
 app.search = Elasticsearch(app.config.get('ELASTICSEARCH_NODES', ['localhost']))
 
 from app import routes
