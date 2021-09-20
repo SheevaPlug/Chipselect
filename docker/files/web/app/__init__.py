ca384b4d0e1bac80e450a750fa4b2851624fdf01
Lutz Badenheuer <software@lukenukem.de>
2021-09-20 20:10:01 +0200

    added recursive output macro for complex values (mappings and sequences)

--- docker/files/web/app/__init__.py
+++ docker/files/web/app/__init__.py
@@ -1,9 +1,9 @@
 from flask import Flask
+from jinja2.tests import test_mapping, test_sequence
 from elasticsearch import Elasticsearch
 
 app = Flask(__name__)
 app.config.from_object('app.config')
 app.search = Elasticsearch(app.config.get('ELASTICSEARCH_NODES', ['localhost']))
-
 
 from app import routes
