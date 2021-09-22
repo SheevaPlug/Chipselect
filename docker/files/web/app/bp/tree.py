9480290f456219921f18e509f0a190e5fe1a26d4
Lutz Badenheuer <software@lukenukem.de>
2021-09-22 20:59:01 +0200

    added pagination to vendor results

--- docker/files/web/app/bp/tree.py
+++ docker/files/web/app/bp/tree.py
@@ -6,17 +6,18 @@ tree = Blueprint('tree', __name__)
 
 @tree.route('/')
 def index():
+    results = app.search.search(
+        index='mcs',
+        body={'size': 0, 'aggs': {'makers': {'terms': {'field': 'vendor.keyword'}}}})
     return render_template(
         'tree/index.html',
-        results=app.search.search(
-            index='mcs',
-            body={'size': 0, 'aggs': {'makers': {'terms': {'field': 'vendor.keyword'}}}}))
+        results=results)
 
 
-@tree.route('/vendor/<name>')
-def vendor(name):
-    return render_template(
-        'tree/vendor.html',
-        results=app.search.search(
-            index='mcs',
-            body={'query': {'term': {'vendor.keyword': name}}}))
+@tree.route('/vendor/<name>', defaults={'start': 0})
+@tree.route('/vendor/<name>/<int:start>')
+def vendor(name, start=0):
+    results = app.search.search(
+        index='mcs',
+        body={'from': start, 'size': app.config.get('PAGINATION', 20), 'query': {'term': {'vendor.keyword': name}}})
+    return render_template('tree/vendor.html', results=results, name=name, start=start)
