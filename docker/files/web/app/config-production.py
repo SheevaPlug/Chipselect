88b836920dd7d0bc525771204b6965999566ee86
Lutz Badenheuer <software@lukenukem.de>
2021-10-06 18:09:17 +0200

    added comment on hostname(s)

--- docker/files/web/app/config-production.py
+++ docker/files/web/app/config-production.py
@@ -3,6 +3,7 @@ SECRET_KEY = '95dee991891bf6d17effcf80e613fa2334de50ea6a9d9410a5b646efafb6a54e'
 ELASTICSEARCH_NODES = ['esearch']
 PAGINATION = 20
 OPENSEARCH_PARAMS = {
+    # @todo  use other hostname here!
     'hosts': [{'host': 'localhost', 'port': 9200}], 
     'http_auth': ('admin', 'admin'),
     'use_ssl': True,
